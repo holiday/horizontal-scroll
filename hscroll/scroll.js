40c2e89263b3966a88ed2e9dd036d21674b106de
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-10-09 14:48:16 -0400

    locking horizontal scroll

--- hscroll/scroll.js
+++ hscroll/scroll.js
@@ -1,7 +1,22 @@
 $(function(){
 
-
+	var currentPageClass = 'section.home';
 	var currentPageIndex = 0;
+
+	function animateTo(pageIndex){
+
+		var offsets = [];
+
+		var w = $(window).width();
+
+		$('section').each(function(i){
+			offsets.push((i * w) - (pageIndex * w));
+		});
+
+		TweenLite.to($('section.home'), 0.5, {left: offsets[0]});
+		TweenLite.to($('section.about'), 0.5, {left: offsets[1]});
+		TweenLite.to($('section.blog'), 0.5, {left: offsets[2]});
+	}
 
 	/**
 	*	Offsets each section tag so they are side by side
@@ -18,37 +33,34 @@ $(function(){
 
     //each time the window resizes we need to set the section offsets again
 	$(window).bind('resize', function(){
-		//setSections();
+		setSections();
 	});
 
 	//listen for menu clicks and slide to the specified section
 	$('.navbar ul li a').click(function(){
 		var page = $(this).attr('class');
 
-
 		var w = window.innerWidth;
 
 		if(page == 'page1'){
 			currentPageIndex = 0;
-			TweenLite.to($('section.home'), 0.5, {left: 0});
-			TweenLite.to($('section.about'), 0.5, {left: w});
-			TweenLite.to($('section.blog'), 0.5, {left: 2 * w});
+			animateTo(0);
 		}
 
 		if(page == 'page2'){
 			currentPageIndex = 1;
-			TweenLite.to($('section.home'), 0.5, {left: -w});
-			TweenLite.to($('section.about'), 0.5, {left: 0});
-			TweenLite.to($('section.blog'), 0.5, {left: w});
+			animateTo(1);
 		}
 
 		if(page == 'page3'){
 			currentPageIndex = 2;
-			TweenLite.to($('section.home'), 0.5, {left: -2 * w});
-			TweenLite.to($('section.about'), 0.5, {left: -w});
-			TweenLite.to($('section.blog'), 0.5, {left: 0});
+			animateTo(2);
 		}
-	})
+	});
+
+	$('section.about').scroll(function(){
+		var x = $(this).scrollTop();
+	});
 
 	//first time section offset setup
 	setSections();
