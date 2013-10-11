7240fee8f4d8be7d7de70a7673d223a9443f65e2
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-10-10 21:13:24 -0400

    refresh now auto slides to page based on hashtag

--- hscroll/js/scroll.js
+++ hscroll/js/scroll.js
@@ -1,18 +1,29 @@
 $(function(){
 
 	var currentPageIndex = 0;
-	var currentPageHash = window.location.hash;
 	var navSelector = '.navbar';
 
+	/**
+	*	Animate a page based on its index (first page is index 0 etc..)
+	*/
 	function animateXTo(pageIndex){
+
+		pageIndex--;
+		currentPageIndex = pageIndex;
 
 		setActive("#nav-bar", pageIndex);
 
-		var w = $(window).width();
+		var w = window.innerWidth;
 
-		$('section').each(function(i){
-			$(this).animate({ left: (i * w) - (pageIndex * w)}, 300);
-		});
+		if (w < 768) {
+			$('.navbar-toggle').click();
+			var aTag = $("a[name='page-"+ (pageIndex + 1) + "']");
+			$('html,body').animate({scrollTop: aTag.offset().top},'fast');
+		}else{
+			$('section').each(function(i){
+				$(this).animate({ left: (i * w) - (pageIndex * w)}, 300);
+			});
+		}		
 	}
 
 	/**
@@ -52,34 +63,39 @@ $(function(){
         }
     }
 
+    /**
+    *	Animate to a page based on the hash in the URL
+    */
+    function forceSectionPosition(hash){
+    	//remove hash tag to get class name
+    	var sectionClassName = hash.substring(1);
+    	//find the section with that class and get its index
+    	var pageIndex = $('section.' + sectionClassName).index();
+    	//animate to that page
+    	animateXTo(pageIndex);
+    }
+
     //each time the window resizes we need to set the section offsets again
 	$(window).bind('resize', function(){
 		setSections();
 	});
 
-	//listen for hash
+	//listen for hash changes
+	$(window).on('hashchange', function(){
+		forceSectionPosition(window.location.hash);
+	})
 
 	//listen for menu clicks and slide to the specified section
 	$(navSelector).find('ul li').click(function(){
-
-		var pageNum = $(this).attr('class').split('-')[1];
-
-		var w = window.innerWidth;
-
-		if (w < 768) {
-			$('.navbar-toggle').click();
-			var aTag = $("a[name='page-"+ pageNum + "']");
-			$('html,body').animate({scrollTop: aTag.offset().top},'fast');
-		}else{
-			currentPageIndex = pageNum - 1;
-			animateXTo(currentPageIndex);
-		}
+		var pageIndex = $(this).attr('class').split('-')[1];
+		animateXTo(pageIndex);
 	});
 
+	//set the default positions of the sections
 	setSections();
+	forceSectionPosition(window.location.hash);
 	
 	/**
-	*	
 	*	Removes current active styles and appends active
 	*	to the correct li element.
 	*/
